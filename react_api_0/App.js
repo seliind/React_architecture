f7a6eb87d77865c15d84102adbed1dacf1726794
Seliin <113685389+seliind@users.noreply.github.com>
2022-11-03 11:11:23 +0300

    Delete react_api_0 directory

--- react_api_0/App.js
+++
@@ -1,7 +0,0 @@
-import React, {useEffect} from "react" ;
-
-export default function App() {
-    fetch('https://webservices.sinavsepeti.com/api/Announcement')
-.then(response => response.json())
-.then(response => console.log(response));
-}
